4f5d14966176476cb254e12920b39c79af577c37
Juan Manuel Barahona <silft@live.com>
2012-06-20 18:04:47 -0400

    Wintergrasp activado completo al menos una gran p
    Wintergrasp activado completo al menos una gran parte volviendo a subir
    los archivos tengo algunos errores con el git :)

---
+++ sql/Updates_sephirot/silft_world_Wintergrasp_20_06_2012.sql
@@ -0,0 +1,9 @@
+/* Adaptando Npc Enginner por  Silft*/
+
+UPDATE creature SET phaseMask = '1' WHERE id = 30400;
+UPDATE creature SET phaseMask = '2' WHERE id = 30499;
+
+/* Adaptando npc guerreros por silft*/
+
+UPDATE creature SET phaseMask = '1' WHERE id = 30740;
+ 
